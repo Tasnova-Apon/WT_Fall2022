e0a7822cc72406bfa20acbad30717b3219bca5aa
Tasnova Ahmed Apon <114417916+Tasnova-Apon@users.noreply.github.com>
2022-11-09 07:49:17 +0600

    Create viewinformation.php

---
+++ User/view/viewinformation.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    
+</head>
+<body bgcolor="grey">
+<center>
+    <tr><td><h1>User Information</h1></td></tr>
+    <tr><?php
+include("../control/viewinformationemp.php");
+?></tr>
+    <tr>
+        <td>
+      <h2><a href="../view/userregistration.php">Back </a></h2>  
+        </td>
+    </tr>
+    </center>
+</body>
+</html>
